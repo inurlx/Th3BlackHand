aa72b21191ace2b1473340cb9d2d81753478feb7
MOHAMMED ADEL <sourceyea@gmail.com>
2020-08-07 17:52:33 +0800

    Update Th3BlackHand.py

--- Th3BlackHand.py
+++ Th3BlackHand.py
@@ -6,12 +6,13 @@ import time
 import ctypes
 
 USER_NAME = getpass.getuser() 
-file_path = "C:/Users/Laptop/AppData/Local/Programs/Python/Python37/python.exe C:/Users/Laptop/Desktop/OutForCoffe/OfC.py" # Modify the paths accordingly 
+file_path = "C:/Users/Laptop/AppData/Local/Programs/Python/Python37/python.exe C:/Users/Laptop/Desktop/Th3BlackHand/Th3BlackHand.py" # Modify the paths accordingly 
 bat_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup' % USER_NAME # i suppose this works for all windows users :) 
+status_path = r'C:\Users\%s\AppData\Roaming\Microsoft\Windows\Start Menu\Programs' % USER_NAME # i suppose this works for all windows users :) 
 with open(bat_path + '\\' + "Th3BlackHand.bat", "w+") as bat_file:
     bat_file.write(r'start "" %s' % file_path)
 
-f = open(bat_path + '\\' + "status.txt", "w")
+f = open(status_path + '\\' + "status.txt", "w")
 f.write("")
 f.close()
 
@@ -31,19 +32,19 @@ while True:
         firebase = firebase.FirebaseApplication("https://test-ed516.firebaseio.com/", None) # Change the firebase url to yours !
         Value = firebase.get('/status/what/', None) # Change the firebase realtime database paths accordingly !!
         if "True" == Value:
-            f = open(bat_path + '\\' + "status.txt", "w")
+            f = open(status_path + '\\' + "status.txt", "w")
             f.write("True")
             f.close()
             print("You Are Allowed :)")
         elif "False" == Value:
-            f = open(bat_path + '\\' + "status.txt", "w")
+            f = open(status_path + '\\' + "status.txt", "w")
             f.write("False")
             f.close()
             ctypes.windll.user32.LockWorkStation()
             print("Not Allowed! :(")
     except OSError:
         print("Internet Connection -> !OK")
-        f = open(bat_path + '\\' + "status.txt", "r")
+        f = open(status_path + '\\' + "status.txt", "r")
         Status = f.read()
         if "True" == Status:
             print("allowed")
